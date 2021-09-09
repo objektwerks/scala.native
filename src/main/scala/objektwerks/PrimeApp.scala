291da2668d0b5fc8a18d10de5f864eeb04b858cd
objektwerks <objektwerks@runbox.com>
2021-09-09 11:14:48 -0400

    refactored prime app since sbt found 2.13

--- src/main/scala/objektwerks/PrimeApp.scala
+++ src/main/scala/objektwerks/PrimeApp.scala
@@ -1,13 +1,12 @@
 package objektwerks
 
 import scala.io.StdIn.readLine
-import scala.util.Try
 
 object PrimeApp {
   def main(args: Array[String]): Unit = {
     println("Enter a number: ")
     val line = readLine()
-    val number = Try(line.toInt).getOrElse(1)
+    val number = line.toIntOption.getOrElse(1)
     println(s"Is $number prime: ${Prime.isPrime(number)}")
   }
 }
