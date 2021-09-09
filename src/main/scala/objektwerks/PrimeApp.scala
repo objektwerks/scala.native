fa6dbfb90635fba6c6eb6ac880998dd3aa939cf2
objektwerks <objektwerks@runbox.com>
2021-09-09 08:35:36 -0400

    corrected class naming error; edited println

--- src/main/scala/objektwerks/PrimeApp.scala
+++ src/main/scala/objektwerks/PrimeApp.scala
@@ -1,9 +1,9 @@
 package objektwerks
 
 import scala.io.StdIn.readLine
-object Primeapp {
+object PrimeApp {
   def main(args: Array[String]): Unit = {
-    println("Provide a number: ")
+    println("Enter a number: ")
     val line = readLine()
     val number = if (line == null) 1 else line.toIntOption.getOrElse(1)
     println(s"Is $number prime: ${Prime.isPrime(number)}")
