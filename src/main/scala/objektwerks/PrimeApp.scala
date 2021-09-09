b334ec98d8849849709f44dedd3fe3d8108000f7
objektwerks <objektwerks@runbox.com>
2021-09-09 09:12:12 -0400

    refactored prime app to use Try on readLine

--- src/main/scala/objektwerks/PrimeApp.scala
+++ src/main/scala/objektwerks/PrimeApp.scala
@@ -1,12 +1,13 @@
 package objektwerks
 
 import scala.io.StdIn.readLine
+import scala.util.Try
 
 object PrimeApp {
   def main(args: Array[String]): Unit = {
     println("Enter a number: ")
     val line = readLine()
-    val number = if (line == null) 1 else line.toIntOption.getOrElse(1)
+    val number = Try(line.toInt).getOrElse(1)
     println(s"Is $number prime: ${Prime.isPrime(number)}")
   }
 }
