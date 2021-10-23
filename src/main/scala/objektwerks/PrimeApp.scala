17343ce5402fe7ce392323658944815de8e86cc3
objektwerks <objektwerks@runbox.com>
2021-10-23 13:09:00 -0400

    enhanced println statements

--- src/main/scala/objektwerks/PrimeApp.scala
+++ src/main/scala/objektwerks/PrimeApp.scala
@@ -4,9 +4,9 @@ import scala.io.StdIn
 
 object PrimeApp {
   def main(args: Array[String]): Unit = {
-    println("Enter a number: ")
+    println("*** Enter a number: ")
     val line = StdIn.readLine()
     val number = if (line == null) 1 else line.toIntOption.getOrElse(1)
-    println(s"Is $number prime: ${Prime.isPrime(number)}")
+    println(s"*** Is $number prime: ${Prime.isPrime(number)}")
   }
 }
