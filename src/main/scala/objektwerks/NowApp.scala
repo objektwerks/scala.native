94d5f1c51e2c9cdef98ca5356fac706023d57314
objektwerks <objektwerks@runbox.com>
2021-10-23 15:08:56 -0400

    enhanced now app with sjavatime

--- src/main/scala/objektwerks/NowApp.scala
+++ src/main/scala/objektwerks/NowApp.scala
@@ -1,9 +1,13 @@
 package objektwerks
 
+import java.time._
 import java.util.Date
 
 object NowApp {
   def main(args: Array[String]): Unit = {
-    println(s"*** Now: ${new Date().toString}")
+    println(s"*** Date: ${new Date().toString}")
+    println(s"*** Instant: ${Instant.now().toString}")
+    println(s"*** Local Date: ${LocalDate.now().toString}")
+    println(s"*** Local Time: ${LocalTime.now().toString}")
   }
 }
