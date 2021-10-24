f759b59ce70a881f055e42c265101d90fbf81237
objektwerks <objektwerks@runbox.com>
2021-10-24 06:32:35 -0400

    implemented now server

--- src/main/scala/objektwerks/NowServer.scala
+++ src/main/scala/objektwerks/NowServer.scala
@@ -1,6 +1,18 @@
 package objektwerks
 
+import java.time.Instant
+
+import snunit._
+
 object NowServer {
   def main(args: Array[String]): Unit = {
+    val server = SyncServerBuilder().withRequestHandler( request =>
+      request.send(
+        statusCode = StatusCode.OK,
+        content = Instant.now().toString,
+        headers = Seq("Content-Type" -> "text/plain")
+      )
+    ).build()
+    server.listen()
   }
 }
